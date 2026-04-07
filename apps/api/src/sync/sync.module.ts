42147651b9edfb6e55a3d84bc27c4cb56e93a980
Wescctech <suporte@wescctech.com.br>
2026-04-06 21:20:15 -0300

    feat: add sync.module.ts

---
+++ apps/api/src/sync/sync.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { SyncService } from './sync.service';
+import { SyncProcessor } from './sync.processor';
+import { SyncController } from './sync.controller';
+import { BullModule } from '@nestjs/bullmq';
+import { PrismaModule } from '../prisma/prisma.module';
+import { IntegrationsModule } from '../integrations/integrations.module';
+import { AuthModule } from '../auth/auth.module';
+import { VismedSyncProcessor } from './vismed-sync/vismed-sync.processor';
+import { VismedService } from '../integrations/vismed/vismed.service';
+import { MappingsModule } from '../mappings/mappings.module';
+import { PushSyncService } from './push-sync.service';
+import { SlotSyncService } from './slot-sync.service';
+
+@Module({
+    imports: [
+        PrismaModule,
+        IntegrationsModule,
+        MappingsModule,
+        AuthModule,
+        BullModule.registerQueue({
+            name: 'vismed-sync',
+        }),
+        BullModule.registerQueue({
+            name: 'sync-queue',
+        }),
+    ],
+    controllers: [SyncController],
+    providers: [SyncService, SyncProcessor, VismedSyncProcessor, VismedService, PushSyncService, SlotSyncService],
+    exports: [SyncService, SlotSyncService],
+})
+export class SyncModule { }
